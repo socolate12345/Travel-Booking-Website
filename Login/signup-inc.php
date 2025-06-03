b60282343e6007449c88750c7c3ef508bf51f18d
socolate12345 <148116422+socolate12345@users.noreply.github.com>
2025-06-03 21:20:37 +0700

    Add files via upload
    Fix the Sign Up function

--- Login/signup-inc.php
+++ Login/signup-inc.php
@@ -1,7 +1,7 @@
 <?php
 
-if (isset($_POST["submit"])){
-    
+if (isset($_POST["submit"])) {
+
     $email = $_POST["email"];
     $username = $_POST["uid"];
     $pwd = $_POST["pwd"];
@@ -10,35 +10,38 @@ if (isset($_POST["submit"])){
     require_once '../dbconnect.php';
     require_once 'functions-inc.php';
 
-    if(emptyInputSignup($email, $username, $pwd, $pwdconfirm) !== false){
-        header("location: ../mainLogin/animlogin.php?error=emptyinput");
+    // Function to show alert and stop script
+    function showAlertAndStop($message) {
+        echo "<script>alert('$message'); window.history.back();</script>";
         exit();
     }
 
-    if(invalidUid($username) !== false){
-        header("location: ../mainLogin/animlogin.php?error=invaliduid");
-        exit();
+    if (emptyInputSignup($email, $username, $pwd, $pwdconfirm) !== false) {
+        showAlertAndStop("Please fill in all fields.");
     }
 
-    if(invalidEmail($email) !== false){
-        header("location: ../mainLogin/animlogin.php?error=invlaidemail");
-        exit();
+    if (invalidUid($username) !== false) {
+        showAlertAndStop("Invalid username format.");
     }
 
-    if(pwdMatch($pwd, $pwdconfirm) !== false){
-        header("location: ../mainLogin/animlogin.php?error=passowrdsdontmatch");
-        exit();
+    if (invalidEmail($email) !== false) {
+        showAlertAndStop("Invalid email address.");
     }
 
-    if(uidExists($conn, $username, $email) !== false){
-        header("location: ../mainLogin/animlogin.php?error=usernametaken");
-        exit();
+    if (pwdMatch($pwd, $pwdconfirm) !== false) {
+        showAlertAndStop("Passwords do not match.");
+    }
+
+    if (uidExists($conn, $username, $email) !== false) {
+        showAlertAndStop("Username or email is already taken.");
     }
 
     createUser($conn, $email, $username, $pwd);
+    echo "<script>alert('Account created successfully!'); window.location.href = '../mainLogin/animlogin.php';</script>";
+    exit();
 
-}
-else{
-    header("location: ../mainLogin/animlogin.php");
+} else {
+    // Direct access to script
+    echo "<script>alert('Invalid access.'); window.location.href = '../mainLogin/animlogin.php';</script>";
     exit();
 }
